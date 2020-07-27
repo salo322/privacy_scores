d5b6abc7fe0956ec2f5f25cc1492e04ef97bcde8
salo322 <salo.sulaberidze7@gmail.com>
2020-07-27 08:11:01 -0700

    update code

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,38 @@
+function requestIcons(){
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let tab = tabs[0];
+    let url = new URL(tab.url)
+    let domain = url.hostname
+     
+      chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+         if (request.message === "getDom"){  
+          chrome.storage.local.set({dmn:domain});
+         }})
+                     
+  chrome.tabs.sendMessage(tabs[0].id, {getDomain: "domains", dm:domain});
+    
+ console.log(domain)
+ 
+if(domain.indexOf('www.') === 0){
+  domain = domain.replace('www.','');
+}
+if(domain.indexOf('web.') === 0){
+  domain = domain.replace('web.','');
+}
+if(domain.indexOf('docs.') === 0){
+  domain = domain.replace('docs.','');
+}  
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"})
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello", dm:domain})
   })
-                 });
- 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {                      
-    if (request.message == "dom"){
-                        
+});                               
 $.ajax({
-  url: `https://api.privacymonitor.com/score?q=${request.domainS}`,
+  url: `https://api.privacymonitor.com/score?q=${domain}`,
   type: 'GET',
   dataType: 'JSON',
   success: function(data, status, xhr) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {status: "success"}, function(response) {
-        console.log(response.farewell);
-      });
-    });
     console.log(data)
     chrome.storage.local.set({st:status});
     chrome.storage.local.set({key:data.score});
@@ -53,12 +68,15 @@ $.ajax({
     chrome.storage.local.set({st:status});
       }
     });
-   
-    
-            }
-          });
-          
+  })
+}
+chrome.tabs.onActivated.addListener(function() {
+ requestIcons();
+})
 
-         
+ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   requestIcons();
+                }); 
 
-             
+                
+ 
--- scripts/content.js
+++ scripts/content.js
@@ -1,77 +1,78 @@
-let address = window.location.hostname;
-if(location.host.indexOf('www.') === 0){
-  address = location.host.replace('www.','');
-}
-if(location.host.indexOf('web.') === 0){
-  address = location.host.replace('web.','');
-}
-if(location.host.indexOf('docs.') === 0){
-  address = location.host.replace('docs.','');
-}
-chrome.runtime.sendMessage({message: "dom",domainS:address});
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-   if (request.greeting == "hello"){   
-    chrome.storage.local.get(['key','prev','st','domSc'], function(result) {
-       if(result.key && result.st === 'success'){
-        function succWindow(){
-          let scoresWindow = `
-        <div class="scWindow">
-          <div class="flexSc">
-          <div class="privacyMonitor">
-            <p class="textSc">Privacy</p>
-            <p class="monitorSc">monitor</p>
-          </div>
-          <div class="x">X</div>
-          </div>
-          <div class="line"></div>
-         <div class="flexFor">
-          <div class="canvasSc">
-            <canvas id="cvs"></canvas>
-         </div>
-         <div class="scoresContainer">
-           <div class="sc">
-          <div class="scText">SCORE:</div>
-          <div class="scores">${result.key}</div>
-           </div>
-         <div class="colorPrevSc" ></div>
-           <div class="trendFlex">
-             <p class="trendSc">Trend:</p>
-             <p class="prevSc">previous scores</p>
-            </div>
-          </div>
-        </div>
-        </div>`
- 
-      $(scoresWindow).appendTo(document.body);
-      $('.scWindow').nextAll('div').remove();
-
-      $('.x').click(function(){
-      $('.scWindow').remove();
-        })
-
-    if(result.key===result.prev){
-      console.log('same')
-      $('.prevSc').text('No change');
-      $('.prevSc').css('color','#336d88')
+   if (request.greeting === "hello"){  
+    let address = window.location.hostname;
+    if(location.host.indexOf('www.') === 0){
+      address = location.host.replace('www.','');
     }
-    
-    if(result.key > result.prev){
-     console.log('increase')
-     $('.prevSc').text('Increase');
-     $('.prevSc').css('color','#ade498')
-   }
-    if(result.key < result.prev){
-     console.log('Decrease')
-     $('.prevSc').text('Decrease');
-     $('.prevSc').css('color','#e84a5f')
-   }
+    if(location.host.indexOf('web.') === 0){
+      address = location.host.replace('web.','');
+    }
+    if(location.host.indexOf('docs.') === 0){
+      address = location.host.replace('docs.','');
+    }
+    console.log(address)
+ chrome.storage.local.get(['key','prev','st','dmn'], function(result) {
+
+  if(result.key && result.st === 'success'){
+    console.log('succ')
+   function succWindow(){
+     let scoresWindow = `
+   <div class="scWindow">
+     <div class="flexSc">
+     <div class="privacyMonitor">
+       <p class="textSc">Privacy</p>
+       <p class="monitorSc">monitor</p>
+     </div>
+     <div class="x">X</div>
+     </div>
+     <div class="line"></div>
+    <div class="flexFor">
+     <div class="canvasSc">
+       <canvas id="cvs"></canvas>
+    </div>
+    <div class="scoresContainer">
+      <div class="sc">
+     <div class="scText">SCORE:</div>
+     <div class="scores">${result.key}</div>
+      </div>
+    <div class="colorPrevSc" ></div>
+      <div class="trendFlex">
+        <p class="trendSc">Trend:</p>
+        <p class="prevSc">previous scores</p>
+       </div>
+     </div>
+   </div>
+   </div>`
+
+ $(scoresWindow).appendTo(document.body);
+ $('.scWindow').nextAll('div').remove();
+
+ $('.x').click(function(){
+ $('.scWindow').remove();
+   })
+
+if(result.key===result.prev){
+ console.log('same')
+ $('.prevSc').text('No change');
+ $('.prevSc').css('color','#336d88')
+}
+
+if(result.key > result.prev){
+console.log('increase')
+$('.prevSc').text('Increase');
+$('.prevSc').css('color','#ade498')
+}
+if(result.key < result.prev){
+console.log('Decrease')
+$('.prevSc').text('Decrease');
+$('.prevSc').css('color','#e84a5f')
+}
 var canvas = document.getElementById("cvs");
 let widthC = "120";
 let heightC="120";
-     canvas.width = widthC;
-     canvas.height = heightC;
+canvas.width = widthC;
+canvas.height = heightC;
 var ctx = canvas.getContext("2d");
 var percent = result.key/10;
 
@@ -117,76 +118,65 @@ ctx.textBaseline = "middle";
 ctx.textAlign = "center";
 ctx.font = "40px arial bold";
 ctx.fillStyle = "#fff"
-          }
-          succWindow();
-       
-        }else if(result.st==='error'){
-          
-          function errWindow(){
-            let window = ` 
-            <div class="window">
+     }
+     succWindow();
+  
+   }else if(result.st==='error'){
+     
+     function errWindow(){
+       console.log('err')
+       let window = ` 
+       <div class="window">
+       <div class="mainFlexErr">
+       <div class="flexErr">
+       <div class="privacyErr">Privacy</div>
+       <div class="monitorErr">monitor</div>
+       </div>
+       <div class="winX">X</div>
+       </div>
+       <div class="line"></div>
+       <div class="gridContainer">
+       <div class="textScore">Sorry, we haven't reviewed ${address} yet.</br>If you would like us to add it to the queue for our legal experts to review, hit the "Request" button now.</div>
+       <div class="buttonContainer">
+       <input class="buttonSc" type="button" value="request" />
+       </div>
+       </div>
+       </div>`
+document.body.innerHTML += window;
+if(result.dmn === this.window.location.hostname){
+  console.log(result.domSc)
+  $('.textScore').html('request sent');
+  $('.buttonSc').css('display','none');
+  setTimeout(()=>{
+    $('.window').remove();
+  },2000)      
+}  
+$('.window').nextAll('div').remove();
+$('.winX').click(function(){
+$('.window').remove();
+})
 
-            <div class="mainFlexErr">
-            <div class="flexErr">
-            <div class="privacyErr">Privacy</div>
-            <div class="monitorErr">monitor</div>
-            </div>
-            <div class="winX">X</div>
-            </div>
-            <div class="line"></div>
-
-            <div class="gridContainer">
-            <div class="textScore">Sorry, we haven't reviewed ${address} yet.</br>If you would like us to add it to the queue for our legal experts to review, hit the "Request" button now.</div>
-            <div class="buttonContainer">
-            <input class="buttonSc" type="button" value="request" />
-            </div>
-            </div>
-            </div>`
-  document.body.innerHTML += window;
-  if(result.domSc === this.window.location.hostname){
-    console.log(result.domSc)
-    $('.textScore').html('request sent');
-    $('.buttonSc').css('display','none');
-    setTimeout(()=>{
-      $('.window').remove();
-    },2000)      
-  }  
-  $('.window').nextAll('div').remove();
-  $('.winX').click(function(){
-  $('.window').remove();
-  })
+$('.window').nextAll('div').remove();
+$('.winX').click(function(){
+$('.window').remove();
+})
 }
-    errWindow();
-      $('.buttonSc').click(function(){
-       $(this).css('display','none');
-      chrome.storage.local.set({domSc:address});
-             
-              $.ajax(`https://api.privacymonitor.com/score?q=${address}`, {
-                success: function(data,status) {
-                if(status==="success"){
-                scoreHtml();
-                }else{
-                
-                $('.textScore').html('request sent');
-                setTimeout(function(){
-                $('.window').css('display','none')
-                },1000)
-                  }
-                   },
-                error: function() {
-                $('.textScore').html('request sent');
-                setTimeout(function(){
-                $('.window').css('display','none')
-                },1000)
-              }
-            })
-          })
+errWindow();
+ $('.buttonSc').click(function(){
+  $(this).css('display','none');
+ setTimeout(()=>{
+  $('.window').remove();
+ },1000)
+ chrome.runtime.sendMessage({message: "getDom"});
+     })
 
-        }else{
-          errWindow();
-        }       
-      })  
-    }
-  })   
+   }else{
+     errWindow();
+   }       
+ })  
 
- 
+
+
+
+   }
+  })
